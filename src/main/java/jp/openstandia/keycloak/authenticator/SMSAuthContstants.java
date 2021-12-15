764b33978e4ab6490865139c003a4fd3e4781b83
Barada456 <iambaradasahoo@gmail.com>
2021-12-15 14:51:32 +0530

    first commit

---
+++ src/main/java/jp/openstandia/keycloak/authenticator/SMSAuthContstants.java
@@ -0,0 +1,14 @@
+package jp.openstandia.keycloak.authenticator;
+
+public class SMSAuthContstants {
+
+	public static final String ATTR_PHONE_NUMBER = "phoneNumber";
+	
+	public static final String COUNTRY_CODE = "countryCode";
+
+	public static final String CONFIG_SMS_API_KEY = "verifySMS.api-key";
+	public static final String CONFIG_PROXY_FLAG = "verifySMS.proxy-flg";
+	public static final String CONFIG_PROXY_URL = "verifySMS.proxy-url";
+	public static final String CONFIG_PROXY_PORT = "verifySMS.proxy-port";
+	public static final String CONFIG_CODE_LENGTH = "verifySMS.code-length";
+}
